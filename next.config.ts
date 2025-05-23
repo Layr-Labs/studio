6d2c5a6c953c8fdcbc5597c0a91aef08e665adb8
Wes Floyd <wesfloyd@gmail.com>
2025-05-23 11:41:16 -0400

    Adding Rate Limiting and Security Headers (#6)

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,28 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self'; script-src 'unsafe-inline' 'report-sample' 'self' https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js; style-src 'unsafe-inline' 'report-sample' 'self'; object-src 'none'; base-uri 'self'; connect-src 'self'; font-src 'self' https://fonts.gstatic.com; frame-src 'self'; img-src 'self'; manifest-src 'self'; media-src 'self'; worker-src 'none';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
